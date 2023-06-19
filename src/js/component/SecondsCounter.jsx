362263fb14a5f2078a5b821e6fb80fd972831e66
bertablancpastor <72865106+bertablancpastor@users.noreply.github.com>
2023-06-19 10:07:52 +0000

    first commit

---
+++ src/js/component/SecondsCounter.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faClock} from '@fortawesome/free-solid-svg-icons';
+
+
+//SecondsCounter Component
+const SecondsCounter = (props) => {
+	return (
+        <>
+        <div className="d-flex bg-black justify-content-center p-5">
+            <div className="card m-1">
+                <div className="card-body">
+                <p className="col-lg-1 display-5 m-0"><FontAwesomeIcon icon = {faClock} /></p>
+                </div>
+            </div>
+            <div className="card m-1 ">
+                <div className="card-body">
+                    <h1 className="card-title">{props.digit6}</h1>
+                </div>
+            </div>
+            <div className="card m-1 ">
+                <div className="card-body">
+                    <h1 className="card-title">{props.digit5}</h1>
+                </div>
+            </div>
+
+            <div className="text-center ">
+                <div className="card-title fs-1 text-light m-2">:</div>
+            </div>
+
+            <div className="card m-1 ">
+                <div className="card-body">
+                    <h1 className="card-title">{props.digit4}</h1>
+                </div>
+            </div>
+            <div className="card m-1 ">
+                <div className="card-body">
+                    <h1 className="card-title">{props.digit3}</h1>
+                </div>
+            </div>
+
+            <div className="text-center ">
+                <div className="card-title fs-1 text-light m-2">:</div>
+            </div>
+
+            <div className="card m-1">
+                <div className="card-body">
+                    <h1 className="card-title">{props.digit2}</h1>
+                </div>
+            </div>
+            <div className="card m-1">
+                <div className="card-body">
+                    <h1 className="card-title text-black">{props.digit1}</h1>
+                </div>
+            </div>
+        </div>
+        </>
+		
+	);
+};
+
+export default SecondsCounter;
+
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,41 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let digit1 = 0;
+let digit2 = 0;
+let digit3 = 0;
+let digit4 = 0;
+let digit5 = 0;
+let digit6 = 0;
+
+setInterval( () => {
+    
+    digit1++;
+
+    if (digit1 === 10){
+        digit1 = 0;
+        digit2++;
+    } if (digit2 === 10){
+        digit2 = 0;
+        digit3++
+    } if ( digit3 === 10){
+        digit3 = 0;
+        digit4++
+    } if (digit4 ===  10){
+        digit4 = 0;
+        digit5++
+    } if (digit5 === 10){
+        digit5 = 0;
+        digit6++
+    } if (digit6 === 10){
+        digit6 = 0;
+    }
+    
+6
+    //render your react application
+    ReactDOM.render(<Home digit1={digit1} digit2={digit2} digit3={digit3} digit4={digit4} digit5={digit5} digit6={digit6} />, document.querySelector("#app"));
+
+
+}, 1000)
+
+
